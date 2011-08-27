e20d720e7e38910b6d25ea501550ae6acd3cfcf9
tim lindner <tlindner@macmess.org>
2011-08-26 19:27:39 -0700

    Evening Commit.

---
+++ Stream/HFAnaylizer.h
@@ -0,0 +1,13 @@
+//
+//  HFAnaylizer.h
+//  Stream
+//
+//  Created by tim lindner on 8/26/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface HFAnaylizer : NSView
+
+@end
---
+++ Stream/HFAnaylizer.m
@@ -0,0 +1,28 @@
+//
+//  HFAnaylizer.m
+//  Stream
+//
+//  Created by tim lindner on 8/26/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "HFAnaylizer.h"
+
+@implementation HFAnaylizer
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
