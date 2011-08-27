c9077cac6e7f1e6cf7ec26589279e662a5fe7bf2
tim lindner <tlindner@macmess.org>
2011-08-26 19:31:02 -0700

    Another commit.

--- Stream/StStream.h
+++ Stream/StStream.h
@@ -1,41 +1,53 @@
 //
-//  StStream.h
+//  Stream.h
 //  Stream
 //
-//  Created by tim lindner on 7/24/11.
+//  Created by tim lindner on 8/25/11.
 //  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
+#import "StAnaylizer.h"
+#import "StBlock.h"
 
-@class StStream;
-
-@interface StStream : NSManagedObject {
+@interface StStream : NSManagedObject
+{
 @private
 }
+
 @property (nonatomic, retain) NSData * bytesAfterTransform;
 @property (nonatomic, retain) NSData * bytesCache;
 @property (nonatomic, retain) NSData * changedBytes;
+@property (nonatomic) float customeSortOrder;
 @property (nonatomic, retain) NSString * displayName;
 @property (nonatomic) NSTimeInterval modificationDateofURL;
 @property (nonatomic, retain) NSString * parentBlock;
-@property (nonatomic, retain) NSData * sourceURL;
+@property (nonatomic, retain) id sourceURL;
+@property (nonatomic, retain) NSString * sourceUTI;
 @property (nonatomic, retain) NSString * streamTransform;
-@property (nonatomic) float customeSortOrder;
-@property (nonatomic, retain) NSSet *anaylizers;
+@property (nonatomic, retain) NSOrderedSet *anaylizers;
 @property (nonatomic, retain) NSSet *blocks;
 @property (nonatomic, retain) NSSet *childStreams;
 @property (nonatomic, retain) StStream *parentStream;
+
+- (NSData *)blockNamed:(NSString *)name;
+- (StBlock *)startNewBlockNamed:(NSString *)name owner:(NSString *)owner;
+
 @end
 
 @interface StStream (CoreDataGeneratedAccessors)
 
-- (void)addAnaylizersObject:(NSManagedObject *)value;
-- (void)removeAnaylizersObject:(NSManagedObject *)value;
-- (void)addAnaylizers:(NSSet *)values;
-- (void)removeAnaylizers:(NSSet *)values;
-
+- (void)insertObject:(StAnaylizer *)value inAnaylizersAtIndex:(NSUInteger)idx;
+- (void)removeObjectFromAnaylizersAtIndex:(NSUInteger)idx;
+- (void)insertAnaylizers:(NSArray *)value atIndexes:(NSIndexSet *)indexes;
+- (void)removeAnaylizersAtIndexes:(NSIndexSet *)indexes;
+- (void)replaceObjectInAnaylizersAtIndex:(NSUInteger)idx withObject:(StAnaylizer *)value;
+- (void)replaceAnaylizersAtIndexes:(NSIndexSet *)indexes withAnaylizers:(NSArray *)values;
+- (void)addAnaylizersObject:(StAnaylizer *)value;
+- (void)removeAnaylizersObject:(StAnaylizer *)value;
+- (void)addAnaylizers:(NSOrderedSet *)values;
+- (void)removeAnaylizers:(NSOrderedSet *)values;
 - (void)addBlocksObject:(NSManagedObject *)value;
 - (void)removeBlocksObject:(NSManagedObject *)value;
 - (void)addBlocks:(NSSet *)values;
